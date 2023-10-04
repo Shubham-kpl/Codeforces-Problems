3294f6a3f027aed8ccb3ff0b7846550cb131abc4
SHUBHAM KANDPAL <102167887+Shubham-kpl@users.noreply.github.com>
2023-10-04 23:00:37 +0530

    Add files via upload

--- D/1851.cpp
+++ D/1851.cpp
@@ -3,35 +3,44 @@
 * कर्मण्येवाधिकारस्ते मा फलेषु कदाचन, मा कर्मफलहेतुर्भुर्मा ते संगोऽस्त्वकर्मणि ॥ *
 
 * The code below is:
-* Coded on : 12/09/2023
+* Coded on : 04/10/2023
 * Coded by: Shubham Kandpal
 
 */
 
 #include <bits/stdc++.h>
 using namespace std;
-// #define int long long
+#define ll long long
 #define pb push_back
-#define f(i, n) for (int i = 0; i < n; i++)
-#define vi(v) vector<int> v
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define vi(v) vector<ll> v
+#define vvi(v) vector<vector<ll>> v
 #define sv(v) sort(v.begin(), v.end())
 #define sa(a) sort(a, a + n)
+#define maxa(a) *max_element(a, a + n)
+#define mina(a) *min_element(a, a + n)
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
 
 void solve()
 {
-    int n;
+    ll n;
     cin >> n;
+
     vi(v)(n);
-    f(i, n) cin >> v[i];
-    int sum = (n * (n + 1)) / 2;
-    cout << "sum " << sum << endl;
+    f(i, n - 1) cin >> v[i];
+
+    // sum
+    ll sum = (n * (n + 1)) / 2;
+
+    // ye bilkul sahi hai
     if (v[n - 2] < sum)
     {
         v[n - 1] = sum;
         vi(diff)(n);
-        set<int> st;
+        set<ll> st;
         diff[0] = v[0], st.insert(diff[0]);
-        for (int i = 1; i < n; i++)
+        for (ll i = 1; i < n; i++)
             diff[i] = v[i] - v[i - 1], st.insert(diff[i]);
 
         if (st.size() != diff.size())
@@ -39,53 +48,37 @@ void solve()
         else
             cout << "YES" << endl;
     }
+
+    // ye bhi shi hai
     else if (v[n - 2] > sum)
     {
         cout << "NO" << endl;
     }
+
     else
     {
-        cout << "HO" << endl;
-        set<int> st;
-        if (v[0] > n)
+        map<ll, ll> mp;
+        mp[v[0]]++;
+        ll cnt = 0; 
+        for (ll i = 1; i < n - 1; i++)
         {
-            int x = 0;
-            for (int i = 1; i < n - 1; i++)
-            {
-                x += v[i] - v[i - 1];
-                st.insert(v[i - 1]);
-            }
-            if (st.size() != n - 2)
-            {
+            mp[v[i] - v[i - 1]]++;
+            if (mp[v[i] - v[i - 1]] > 1) cnt++;
+            if(cnt > 1) {
                 cout << "NO" << endl;
                 return;
             }
-            if (sum - x == v[0])
-                cout << "YES" << endl;
-            else
+            if(mp[v[i] - v[i - 1]] > 2) {
                 cout << "NO" << endl;
-        }
-        else
-        {
-            st.insert(v[0]);
-            int save = v[0], rem;
-            for (int i = 1; i < n - 1; i++)
-            {
-                if (v[i] - v[i - 1] <= n)
-                    save += (v[i] - v[i - 1]), st.insert(diff[i]);
-                else
-                    rem = v[i] - v[i - 1];
-            }
-            if (st.size() != n - 1)
-                cout << "NO" << endl;
-            else
-            {
-                if (rem == sum - save)
-                    cout << "YES" << endl;
-                else
-                    cout << "NO" << endl;
+                return;
             }
         }
+        cnt = 0;
+        for(ll i = 1; i <= n; i++) {
+            if(mp[i] == 0) cnt++;
+        }
+        if(cnt != 2) cout << "NO" << endl;
+        else cout << "YES" << endl;
     }
 }
 
