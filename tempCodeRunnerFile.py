e3b77e404c070d88c8648ab7caf3933ca79e3ab3
Shubham-Kpl <codewithsk27@gmail.com>
2024-06-11 15:38:00 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-is_safe_state, safe_sequence = is_safe(processes, avail, max_need, allocation)
-if is_safe_state:
-    print("System is in a safe state.")
-    print("Safe sequence is:", safe_sequence)
-else:
-    print("System is not in a safe state.")
